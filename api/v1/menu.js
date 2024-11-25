53eef0649c6b2281e1142248f1613a2d23598795
Pogreba <500192896@fvtc.edu>
2024-11-25 16:19:12 -0600

    Menu updating

--- api/v1/menu.js
+++ api/v1/menu.js
@@ -5,7 +5,8 @@ const { getCollection, ObjectId } = require('../../dbconnect')
 // routes
 router.get('/menu', async (req, res) => {
     const collection = await getCollection('FoodTruckApi', 'Menu')
-    res.send( await collection.findOne( {"Name": "Cheeseburger"} ))
+    
+    res.send(collection)
 })
 
 module.exports = router
