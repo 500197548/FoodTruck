9ff8cb9e7a1cb23847dff1e17a4d25b3358ae5b6
Bloedorn <500194031@fvtc.edu>
2024-11-25 15:12:42 -0600

    Added MongoDB

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3011
+const { getCollection, ObjectId } = require('./dbconnect.js')
 
+// middleware
+app.use(express.static('public'))
+app.use(express.json())
+
+// routes
+app.get('/test', async (req, res) => {
+    const collection = await getCollection('FoodTruckApi', 'Events')
+    res.send( await collection.findOne( {"Name": "Taco Tuesday"} ))
+})
+
+
+app.listen(port, () => console.log(`Running at: http://localhost:${port}`))
