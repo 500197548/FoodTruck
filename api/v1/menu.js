fc72e9f62fb8f6d68481695bfc17983033aad536
Pogreba <500192896@fvtc.edu>
2024-11-25 17:20:33 -0600

    Menu updating

--- api/v1/menu.js
+++ api/v1/menu.js
@@ -3,10 +3,19 @@ const router = require('express').Router()
 const { getCollection, ObjectId } = require('../../dbconnect')
 
 // routes
-router.get('/menu', async (req, res) => {
+router.get('/menu', async (request, response) => {
     const collection = await getCollection('FoodTruckApi', 'Menu')
-    
-    res.send(collection)
+    const found = await collection.find().toArray()
+    const results = {"Menu": found}
+    response.send(results)
+})
+
+router.get('/menu/:id', async (request, response) => {
+    const { id } = request.params
+    const collection = await getCollection('FoodTruckApi', 'Menu')
+    const found = await collection.findOne({ "id": parseInt(id) })
+    if (found) response.send(found)
+    else response.send({ error: { message: `Could not find Menu Item with id: ${id}` }})
 })
 
 module.exports = router
