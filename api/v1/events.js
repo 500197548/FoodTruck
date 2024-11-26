41ab488eabe158795dfcac03a34dbdff3da8b322
Pogreba <500192896@fvtc.edu>
2024-11-25 20:33:15 -0600

    Finished Menu.js and /events/add.

--- api/v1/events.js
+++ api/v1/events.js
@@ -10,5 +10,16 @@ router.get('/events', async (req, res) => {
     
     res.send(results)
 })
+router.post('/events/add', async (request, response) => {  
+    const { Name, Location, Date, Time} = request.body
+    const collection = await getCollection('FoodTruckApi', 'Events')
+    const events = await collection.find().toArray();
+
+    const found = events.find(c => c.Name.toString() === Name.toString())
+    const newId = events.length + 1
+    if (found) response.send({ error: { message: `Menu Item: ${Name}, already exists`} })
+    else collection.insertOne({Name, Location, Date, Time, id: newId}), response.send({message: {message: `Menu item added: ${Name}, ${Location}, ${Date}, ${Time}`}})
+
+})
 
 module.exports = router
--- api/v1/menu.js
+++ api/v1/menu.js
@@ -5,7 +5,7 @@ const { getCollection, ObjectId } = require('../../dbconnect')
 // routes
 router.get('/menu', async (request, response) => {
     const collection = await getCollection('FoodTruckApi', 'Menu')
-    const found = await collection.find().toArray()
+    const found = await collection.find({}).toArray()
     const results = {"Menu": found}
     response.send(results)
 })
@@ -18,15 +18,17 @@ router.get('/menu/:id', async (request, response) => {
     else response.send({ error: { message: `Could not find Menu Item with id: ${id}` }})
 })
 
-router.post('/menu/add', async (request, response) => {
+router.post('/add', async (request, response) => {  
+    const { Name, Description, Price, Url} = request.body
     
-    const collection = await getCollection()
-    const { Name, Description, Price, Url } = request.body
-    console.log({  Name, Description, Price, Url  })
-    const found = collection.find(p => p.Name.toString() === Name.toString())
-    const newId = collection.length + 1
+    const collection = await getCollection('FoodTruckApi', 'Menu')
+    const menu = await collection.find().toArray();
+    console.log(menu)
+    const found = menu.find(c => c.Name.toString() === Name.toString())
+    const newId = menu.length + 1
     if (found) response.send({ error: { message: `Menu Item: ${Name}, already exists`} })
-    else recipes.push({ id: newId, Name, Description, Price, Url}), response.send({message: {message: "Menu Item Added"}})
+    else collection.insertOne({Name, Description, Price, Url, id: newId}), response.send({message: {message: `Event Scheduled ${Name}, ${Description}, ${Price}, ${Url}`}})
+    console.log(menu)
 })
 
 module.exports = router
