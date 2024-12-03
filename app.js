aeeb9748a85f9657a19124635ec35b834b2ec78d
unknown <jonathan@bloedornfamily.com>
2024-12-03 16:01:51 -0600

    Started the /admin front-end route

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3011
 const { getCollection, ObjectId } = require('./dbconnect.js')
+const path = require('path')
+const root = path.join(__dirname, 'public')
 
 // middleware
 app.use(express.static('public'))
@@ -13,6 +15,10 @@ app.use('/api/v1', require('./api/v1/events.js'))
 app.get('/test', async (req, res) => {
     const collection = await getCollection('FoodTruckApi', 'Events')
     res.send( await collection.findOne( {"Name": "Taco Tuesday"} ))
+})
+
+app.get('/admin', async (req, res) => {
+    res.sendFile('add_form.html', { root }) // The front-end form for adding new menu items and events
 })
 
 app.listen(port, () => console.log(`Running at: http://localhost:${port}`))
