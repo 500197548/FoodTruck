8d6b8c84c42c3e9fbe240ff184b5ee23ec555091
unknown <jonathan@bloedornfamily.com>
2024-12-03 21:19:19 -0600

    /admin page core functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const root = path.join(__dirname, 'public')
 // middleware
 app.use(express.static('public'))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1', require('./api/v1/menu.js'))
 app.use('/api/v1', require('./api/v1/events.js'))
 
@@ -17,8 +18,38 @@ app.get('/test', async (req, res) => {
     res.send( await collection.findOne( {"Name": "Taco Tuesday"} ))
 })
 
-app.get('/admin', async (req, res) => {
-    res.sendFile('add_form.html', { root }) // The front-end form for adding new menu items and events
+app.get('/admin', (req, res) => {
+    res.sendFile('add_form.html', { root })
+})
+
+app.post('/admin/addMenuItem', async (req, res) => {
+    const {Name, Description, Price, Url} = req.body
+    const apiUrl = `http://localhost:${port}/api/v1/add`
+    
+    const result = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({Name, Description, Price, Url})
+    })
+
+    res.redirect('/admin')
+})
+
+app.post('/admin/addEvent', async (req, res) => {
+    const {Name, Location, Date, Time} = req.body
+    const apiUrl = `http://localhost:${port}/api/v1/events/add`
+    
+    const result = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({Name, Location, Date, Time})
+    })
+
+    res.redirect('/admin')
 })
 
 app.listen(port, () => console.log(`Running at: http://localhost:${port}`))
