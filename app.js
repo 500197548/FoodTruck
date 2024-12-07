3a623481d79143ce89b24341463d63f245bec1cc
unknown <jonathan@bloedornfamily.com>
2024-12-07 16:20:42 -0600

    Tweaked the /admin functionality to format and validate the form data to match what's already in the DB

--- app.js
+++ app.js
@@ -24,9 +24,15 @@ app.get('/admin', (req, res) => {
 })
 
 app.post('/admin/addMenuItem', async (req, res) => {
-    const {Name, Description, Price, Url} = req.body
+    const {Name, Description, Price : priceInput, Url} = req.body
     const apiUrl = `http://localhost:${port}/api/v1/add`
     
+    // Convert the price to a number if possible
+    try {
+        Price = parseFloat(priceInput)
+    } catch { console.log('Error: Price is not a number.') }
+
+    // Push the data to the DB
     const result = await fetch(apiUrl, {
         method: 'POST',
         headers: {
@@ -39,15 +45,32 @@ app.post('/admin/addMenuItem', async (req, res) => {
 })
 
 app.post('/admin/addEvent', async (req, res) => {
-    const {Name, Location, Date, Time} = req.body
+    const {Name, Location, Date: dateInput, Time: timeInput} = req.body
     const apiUrl = `http://localhost:${port}/api/v1/events/add`
     
+    //-- Reformat Date to match our DB --//
+    const DateTime = new Date(`${dateInput}T${timeInput}`)
+    const month = (DateTime.getMonth() + 1).toString().padStart(2, '0')
+    const day = DateTime.getDate().toString().padStart(2, '0')
+    const year = DateTime.getFullYear()
+    const hour = DateTime.getHours()
+    const minute = DateTime.getMinutes().toString().padStart(2, '0')
+
+    // Change hours from 24 hour time to 12 hour time
+    const period = hour >= 12 ? "PM" : "AM"
+    const hourReformatted = hour % 12 || 12
+
+    // Decides how the date and time will be sent to the DB
+    const dateReformatted = `${month}/${day}/${year}`
+    const timeReformatted = `${hourReformatted}:${minute}${period}`
+
+    // Push the data to the DB
     const result = await fetch(apiUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({Name, Location, Date, Time})
+        body: JSON.stringify({Name, Location, Date: dateReformatted, Time: timeReformatted})
     })
 
     res.redirect('/admin')
